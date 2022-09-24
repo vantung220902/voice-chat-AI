3fa1f0d0ef406af9196c44415cf0cb708577dc15
Tung Nguyen <tung_nguyen@datahouse.com>
2022-09-24 10:31:40 +0700

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: 'env',
+        }),
+      ],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        synchronize: configService.get<boolean>('DB_SYNCHRONIZE'),
+        entities: [__dirname + '/../**/*.entity.js'],
+        logging: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [],
+  controllers: [],
+})
+export class AppModule {}
